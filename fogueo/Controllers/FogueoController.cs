4bac7b1d4e2e0d54664c1af8ae620721573cee76
carlosyael <62651008+carlosyael@users.noreply.github.com>
2022-02-17 22:03:10 -0400

    proyecto 2/3/2022

---
+++ fogueo/Controllers/FogueoController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace fogueo.Controllers
+{
+    public class FogueoController : Controller
+    {
+        // GET: Fogueo
+        public ActionResult asp()
+        {
+            return View();
+        }
+        public ActionResult pilares()
+        {
+            return View();
+        }
+        public ActionResult presentacion()
+        {
+            return View();
+        }
+        public ActionResult itla()
+        {
+            return View();
+        }
+        public ActionResult fomulario()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ fogueo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="fogueo.MvcApplication" Language="C#" %>
